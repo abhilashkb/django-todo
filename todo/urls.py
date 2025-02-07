4f9fa4aeab37a481908d4cd595305681ed1de981
abhilashkb <abhilashkb@outlook.com>
2025-02-07 13:29:37 +0530

    2nd commit

---
+++ todo/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.list_tasks, name='list_tasks'),
+    path('create/', views.create_task, name='create_task'),
+    path('update/<int:task_id>/', views.update_task, name='update_task'),
+]
