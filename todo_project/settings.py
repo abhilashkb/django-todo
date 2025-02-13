21bdf663796de2571ac5565f910caacca15b01c3
abhilashkb <abhilashkb@outlook.com>
2025-02-13 18:55:39 +0530

    dockerfile added

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8e!64u4sv$462y3n+c=hai3##(bz!5a+&b0fxnqzhd2+dsaa2b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
